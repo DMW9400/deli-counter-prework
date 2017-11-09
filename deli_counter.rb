663c9b5edd126031d5ed728989d959715c127232
David Welch <dmw9400@gmail.com>
2017-11-09 20:07:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def line(queue)
   line_place = 1
   i = 0
   return_string = "The line is currently: "
-  final_string = 
+  final_string = queue(-1)
 
   if queue.length == 0
     puts "The line is currently empty."
@@ -29,7 +29,7 @@ def line(queue)
       line_place+=1
       i++
       return_string += new_string
-      
+
     end
     puts return_string
   end

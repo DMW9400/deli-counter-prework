95d63a06fdc457226b8aaa0b8736f6e5dfb8b267
David Welch <dmw9400@gmail.com>
2017-11-09 19:52:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(queue)
   line_place = 1
   if queue.length == 0
   return_string = "The line is currently: "
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     queue.each do |item|
       new_string = "line_place. #{item}"

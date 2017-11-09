42ac9f81671a6ff80b432ff07e95fc314e934d56
David Welch <dmw9400@gmail.com>
2017-11-09 19:46:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
 def line(queue)
   line_place = 1
   if queue.length == 0
+  return_string = "The line is currently: "
     return "The line is currently empty."
   else
+    while
     return "The line is currently: "
 end
